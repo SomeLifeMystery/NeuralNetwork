72719582cba7a9abdc578b656a5817f91ed4d1e4
Hadrien Daures <hadrien.daures@gmail.com>
2017-06-19 16:44:24 +0200

    changed trainingData_xor to trainingData_xor_and_or

--- examples/xor_and_or.cpp
+++ examples/xor_and_or.cpp
@@ -8,31 +8,31 @@ int main() {
   try {
     NeuralNetwork::MLP p(5);
 
-    std::vector< std::pair< std::vector<double>, std::vector<double> > > trainingData_xor = {
+    std::vector< std::pair< std::vector<double>, std::vector<double> > > trainingData_xor_and_or = {
       { { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0 } },
       { { 0.0, 1.0, 0.0, 1.0, 0.0, 1.0 }, { 1.0, 0.0, 1.0 } },
       { { 1.0, 0.0, 1.0, 0.0, 1.0, 0.0 }, { 1.0, 0.0, 1.0 } },
       { { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 }, { 0.0, 1.0, 1.0 } }
     };
-    std::vector<double> outputs = trainingData_xor[0].second;
+    std::vector<double> outputs = trainingData_xor_and_or[0].second;
 
-    for (int i = trainingData_xor[0].first.size(); i > 0; --i) {
+    for (int i = trainingData_xor_and_or[0].first.size(); i > 0; --i) {
       p.addNeuron(0);
       p.addNeuron(1);
       p.addNeuron(2);
       p.addNeuron(3);
     }
-    for (int i = trainingData_xor[0].second.size(); i > 0; --i) {
+    for (int i = trainingData_xor_and_or[0].second.size(); i > 0; --i) {
       p.addNeuron(4);
     }
   
     for (int i = 0; i < 1000; ++i) {
-      for (int j = 0; j < trainingData_xor.size(); ++j) {
-	double error = p.train(trainingData_xor[j].first, outputs, trainingData_xor[j].second);
+      for (int j = 0; j < trainingData_xor_and_or.size(); ++j) {
+	double error = p.train(trainingData_xor_and_or[j].first, outputs, trainingData_xor_and_or[j].second);
 	std::cout << error << std::endl;
       }
     }
-    for (const auto& trainingData : trainingData_xor) {
+    for (const auto& trainingData : trainingData_xor_and_or) {
       p.process(trainingData.first, outputs);
       std::cout << "inputs(";
       for (const auto& trainingDataInput : trainingData.first) std::cout << trainingDataInput << ", ";
